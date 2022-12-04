a4fe063fc3fa340081e0ce110efbc8484a4b754b
hemicontinuous <119049501+hemicontinuous@users.noreply.github.com>
2022-12-04 13:43:30 +0000

    Update diagnostics.r

--- R/R/diagnostics.r
+++ R/R/diagnostics.r
@@ -1,7 +1,7 @@
 ## check for missing packages and install them
 list.of.packages <- c("ggplot2", "goftest","nortest", "openxlsx", "readxl",
                       "RTransferEntropy", "ggpubr","purrr",
-                      "tseries", "fastmatrix", "genridge", "car" )
+                      "tseries", "fastmatrix", "genridge")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 set.seed(123)
@@ -15,7 +15,7 @@ library(purrr)
 library(tseries)
 library(fastmatrix)
 library(genridge)
-library(car)
+# library(car)
 # library(goftest)
 # install.packages("fastmatrix")
 # install.packages("genridge")
